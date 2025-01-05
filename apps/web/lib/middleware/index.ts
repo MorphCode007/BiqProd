852e658ed579480022cb9ba221286cfb6277c829
Ayoub Boussaber <114665037+MorphCode007@users.noreply.github.com>
2025-01-06 00:06:03 +0100

    Update index.ts

--- apps/web/lib/middleware/index.ts
+++ apps/web/lib/middleware/index.ts
@@ -1,5 +1,5 @@
 export { default as AdminMiddleware } from "./admin";
-export { default as ApiMiddleware } from "./api";
+//export { default as ApiMiddleware } from "./api";
 export { default as AppMiddleware } from "./app";
 //export { default as AxiomMiddleware } from "./axiom";
 export { default as CreateLinkMiddleware } from "./create-link";
